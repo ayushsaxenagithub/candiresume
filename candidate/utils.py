5c56513924ba38637dd0e896ff3ac84012d810c6
Ayush Saxena <71148650+ayushsaxena01@users.noreply.github.com>
2022-05-20 14:36:08 +0530

    env add

--- candidate/utils.py
+++ candidate/utils.py
@@ -1,35 +1,7 @@
 from django.db.models import Q
-from .models import Candidate, Skill, Project, Tag
-
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from .models import Candidate, Skill, Project
 
 
-# def paginateProfiles(request, profiles, results):
-#     page = request.GET.get('page')
-#     paginator = Paginator(profiles, results)
-
-#     try:
-#         profiles = paginator.page(page)
-#     except PageNotAnInteger:
-#         page = 1
-#         profiles = paginator.page(page)
-#     except EmptyPage:
-#         page = paginator.num_pages
-#         profiles = paginator.page(page)
-
-#     leftIndex = (int(page) - 4)
-
-#     if leftIndex < 1:
-#         leftIndex = 1
-
-#     rightIndex = (int(page) + 5)
-
-#     if rightIndex > paginator.num_pages:
-#         rightIndex = paginator.num_pages + 1
-
-#     custom_range = range(leftIndex, rightIndex)
-
-#     return custom_range, profiles
 
 
 def searchProfiles(request):
